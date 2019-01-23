c719ec8fe8389e45db5d2dba893541dfdd86319b
bhargavayyagari <bbhaargav@gmail.com>
2019-01-22 21:15:36 -0600

    solved problems in c10

---
+++ programming/ctci/v1/c10/sorted_anagrams.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        strings = ["abed", "later", "bead", "alert", "altered", "bade", "alter", "alerted"]
+        actual = sorted_anagrams(strings)
+        print(actual)
+
+
+def sorted_anagrams(strings):
+    anagrams = set_anagrams(strings)
+    index = 0
+    for k, v in anagrams.items():
+        for value in anagrams[k]:
+            strings[index] = value
+            index += 1
+    return strings
+
+
+def set_anagrams(strings):
+    anagrams = {}
+    for i in strings:
+        word = "".join(sorted(i.lower()))
+        if word not in anagrams:
+            anagrams.setdefault(word, [])
+        anagrams[word].append(i)
+    return anagrams
+
+
+if __name__ == '__main__':
+    unittest.main()
