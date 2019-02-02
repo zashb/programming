12b1b059119a45c00b3f0e750ba3c436d39600ff
bhargavayyagari <bbhaargav@gmail.com>
2019-02-02 07:12:25 -0600

    revisiting c8 - parenthesis

--- programming/ctci/v1/c8/parenthesis.py
+++ programming/ctci/v1/c8/parenthesis.py
@@ -47,6 +47,39 @@ def is_valid(string):
     return ctr == 0
 
 
+def generate_parenthesis(n):
+    """Intuition:
+    To enumerate something, generally we would like to express it as a sum of disjoint subsets that are easier to count.
+    Consider the closure number of a valid parentheses sequence S: the least index >= 0 so that S[0], S[1], ..., S[2*index+1] is valid. Clearly, every parentheses sequence has a unique closure number. We can try to enumerate them individually.
+
+    Algorithm:
+    For each closure number c, we know the starting and ending brackets must be at index 0 and 2*c + 1. Then, the 2*c elements between must be a valid sequence, plus the rest of the elements must be a valid sequence."""
+    if n == 0:
+        return ['']
+    ans = []
+    for c in range(n):
+        for left in generate_parenthesis(c):
+            for right in generate_parenthesis(n - 1 - c):
+                ans.append('({}){}'.format(left, right))
+    return ans
+
+
+def generate_parenthesis2(n):
+    res = []
+    generater(res, 2 * n, "", n, n)
+    return res
+
+
+def generater(res, n, tmp, left_rem, right_rem):
+    if left_rem < 0 or left_rem > right_rem:
+        return
+    if left_rem == 0 and right_rem == 0:
+        res.append(tmp)
+        return
+    generater(res, n - 1, tmp + "(", left_rem - 1, right_rem)
+    generater(res, n - 1, tmp + ")", left_rem, right_rem - 1)
+
+
 class Test(unittest.TestCase):
     def test_vp(self):
         cases = ["()", "()[]{}", "(]", "([)]", "{[]}"]
@@ -65,3 +98,10 @@ class Test(unittest.TestCase):
         for case in cases:
             actual = remove_invalid_par(case)
             print("removing invalid parenthesis for {} gives : {}".format(case, actual))
+
+    def test_all_parens(self):
+        n = 3
+        actual = generate_parenthesis(n)
+        print("generated parenthesis for n={} are : {}".format(n, actual))
+        actual = generate_parenthesis2(n)
+        print("generated parenthesis for n={} are : {}".format(n, actual))
