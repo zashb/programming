8cafdcc8bcafbca282cdc1fb9b01cd8976fcf47a
bhargavayyagari <bbhaargav@gmail.com>
2019-03-04 08:03:48 -0600

    prgcrk - strings

--- programming/datstr/prgcrk/1string/py/29minWinSubstr.py
+++ programming/datstr/prgcrk/1string/py/29minWinSubstr.py
@@ -1,21 +1,32 @@
 import collections
-# find the minimum window in s which will contain all the characters in t
 
-def minWinSubstr(s,subStr):
-	need, missing = collections.Counter(subStr), len(subStr)
-	i = I = J = 0
-	for j in range(len(s)):
-		missing-=need[s[j]]>0
-		need[s[j]]-=1
-		if not missing:
-			while i<j and need[s[i]]<0:
-				need[s[i]]+=1
-				i+=1
-			if not J or j-i<=J-I:	I,J=i,j
-	return s[I:J+1]
+
+# find the minimum window in s which will contain all the characters in t
+def minWinSubstr(string, substring):
+    substring_counter, n_substring = collections.Counter(substring), len(substring)
+    i, I, J = 0, 0, 0
+    for j in range(len(string)):
+        if substring_counter[string[j]] > 0:
+            n_substring = n_substring - substring_counter[string[j]]
+        substring_counter[string[j]] = substring_counter[string[j]] - 1
+        if n_substring == 0:
+            while i < j and substring_counter[string[i]] < 0:
+                substring_counter[string[i]] = substring_counter[string[i]] + 1
+                i += 1
+            if J == 0 or j - i <= J - I:
+                I, J = i, j
+    return string[I:J + 1]
+
 
 if __name__ == "__main__":
-	print(minWinSubstr("ADOBECODEBANC","ABC"))
+    string = "ADOBECODEBANC"
+    substring = "ABC"
+    expected = "BANC"
+    actual = minWinSubstr(string, substring)
+    print(actual == expected)
 
-n,m=collections.Counter(subStr),len(subStr)
-i=I=J=0
+    string = "geeksforgeeks"
+    substring = "ork"
+    expected = "ksfor"
+    actual = minWinSubstr(string, substring)
+    print(expected == actual)
--- programming/datstr/prgcrk/1string/py/35bullsCows.py
+++ programming/datstr/prgcrk/1string/py/35bullsCows.py
@@ -1,20 +1,47 @@
-def bullsCows(secret,guess):
-    bc,cc,l = 0,0,[0]*10
+# def bulls_cows(secret, guess):
+#     bull_count, cow_count = 0, 0
+#     n = len(secret)
+#     for i in range(n):
+#         if secret[i] == guess[i]:
+#             bull_count = bull_count + 1
+#     for i in guess:
+#         if i in secret and guess.index(i) != secret.index(i):
+#             cow_count = cow_count + 1
+#     return "{}A{}B".format(bull_count, cow_count)
+#
+#
+# secret, guess = "1807", "7810"
+# expected = "1A3B"
+# actual = bulls_cows(secret, guess)
+# print(expected == actual)
+
+def bullsCows(secret, guess):
+    bc, cc = 0, 0
+    ord_flag = [0] * 126
     for i in range(len(secret)):
-        secret_i,guess_i = int(secret[i]),int(guess[i])
-        if secret_i == guess_i:  bc += 1
+        s, g = secret[i], guess[i]
+        if s == g:
+            bc += 1
         else:
-            if l[secret_i]<0: cc+=1
-            if l[guess_i]>0: cc+=1
-            l[secret_i]+=1
-            l[guess_i]-=1
-    return str(bc)+"A"+str(cc)+"B"
+            ord_s, ord_g = ord(s), ord(g)
+            # the opposite of base condition
+            if ord_flag[ord_s] < 0:
+                cc += 1
+            if ord_flag[ord_g] > 0:
+                cc += 1
+            ord_flag[ord_s] = ord_flag[ord_s] + 1
+            ord_flag[ord_g] = ord_flag[ord_g] - 1
+    return "{}A{}B".format(bc, cc)
 
-if __name__=="__main__":
-    print(bullsCows("1807","7810"))
-    print(bullsCows("1234","7210"))
 
-# notes
-# init l: a list to inc/dec val at int(i)
-# bc +=1 straight away
-# cc +=1 if <0 or >0
+if __name__ == "__main__":
+    secret = "1807"
+    guess = "7810"
+    expected = "1A3B"
+    actual = bullsCows(secret, guess)
+    print(expected == actual)
+    secret = "ABC"
+    guess = "BDC"
+    expected = "1A1B"
+    actual = bullsCows(secret, guess)
+    print(expected == actual)
