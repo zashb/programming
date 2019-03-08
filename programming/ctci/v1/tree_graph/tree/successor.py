4e251c0c2bca94faf7f224ee1b3c438f99bb2ed5
bhargavayyagari <bbhaargav@gmail.com>
2019-03-08 10:30:11 -0600

    gfg top10 - bst successor

--- programming/ctci/v1/tree_graph/tree/successor.py
+++ programming/ctci/v1/tree_graph/tree/successor.py
@@ -1,66 +1,62 @@
 import unittest
 
 
-class Node:
-    def __init__(self, key):
-        self.data = key
+class Tree_Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
         self.left = None
         self.right = None
 
 
-def get_successor(n):
-    if n.right:
-        return minv(n.right)
-    p = n.parent
-    while p:
-        if n != p.right:
-            break
-        n = p
-        p = p.parent
-    return p
+def get_inorder(root):
+    if root is None:
+        return None
+    get_inorder(root.left)
+    print(root.val)
+    get_inorder(root.right)
 
 
-def minv(node):
-    current = node
-    while current is not None:
-        if current.left is None:
-            break
-        current = current.left
-    return current
-
-
-def insert(node, data):
-    if node is None:
-        return Node(data)
+def insert(root, val):
+    if root is None:
+        root = Tree_Node(val)
     else:
-        if data <= node.data:
-            temp = insert(node.left, data)
-            node.left = temp
-            temp.parent = node
+        if val > root.val:
+            if root.right is None:
+                root.right = Tree_Node(val)
+            else:
+                insert(root.right, val)
         else:
-            temp = insert(node.right, data)
-            node.right = temp
-            temp.parent = node
-        return node
+            if root.left is None:
+                root.left = Tree_Node(val)
+            else:
+                insert(root.left, val)
+
+
+def get_io_successor(root, target):
+    candidate = None
+    while root:
+        if root.val == target:
+            if root.right is not None:
+                candidate = root.right
+                while candidate.left is not None:
+                    candidate = candidate.left
+            root = None
+        elif target < root.val:
+            candidate = root
+            root = root.left
+        else:
+            root = root.right
+    return candidate.val
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        root = None
-        root = insert(root, 20)
-        root = insert(root, 8)
-        root = insert(root, 22)
-        root = insert(root, 4)
-        root = insert(root, 12)
-        root = insert(root, 10)
-        root = insert(root, 14)
-        temp = root.left.right.right
-        succ = get_successor(temp)
-        if succ is not None:
-            print("\nInorder Successor of %d is %d " % (temp.data, succ.data))
-        else:
-            print("\nInorder Successor doesn't exist")
-
-
-if __name__ == '__main__':
-    unittest.main()
+        root = Tree_Node(20)
+        insert(root, 8)
+        insert(root, 22)
+        insert(root, 4)
+        insert(root, 12)
+        insert(root, 10)
+        insert(root, 14)
+        succ = get_io_successor(root, 14)
+        print(succ)
