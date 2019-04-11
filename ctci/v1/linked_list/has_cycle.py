1a7741bd3457eb2ca58bee68d158a3e221f869f3
bhargavayyagari <bbhaargav@gmail.com>
2019-04-11 06:28:35 -0500

    linkedlist loop

---
+++ ctci/v1/linked_list/has_cycle.py
@@ -0,0 +1,27 @@
+"""
+prob:
+idea:
+comp:
+"""
+from ctci.v1.linked_list.linked_list import LinkedList, LinkedListNode
+
+
+def has_cycle(head):
+    if not head or not head.next:
+        return False
+    slow, fast = head, head.next
+    while slow != fast:
+        if not fast or not fast.next:
+            return False
+        slow, fast = slow.next, fast.next.next
+    return True
+
+
+ll = LinkedList()
+ll.head = LinkedListNode(1)
+ll.head.next = LinkedListNode(2)
+ll.head.next.next = LinkedListNode(3)
+ll.head.next.next = ll.head.next
+expected = True
+actual = has_cycle(ll.head)
+print(expected == actual)
