99379b15268ee900586152141a0b3ada08323ece
bhargavayyagari <bbhaargav@gmail.com>
2019-04-29 19:29:24 -0500

    dp - coin change

--- prgcrk/dp/coin_change.py
+++ prgcrk/dp/coin_change.py
@@ -9,9 +9,9 @@ def coin_change(coins, amount):
     dp = [float('inf')] * (amount + 1)
     dp[0] = 0
     for i in range(1, amount + 1):
-        for j in range(len(coins)):
-            if coins[j] <= i:
-                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+        for j in coins:
+            if j <= i:
+                dp[i] = min(dp[i], dp[i - j] + 1)
     return -1 if dp[amount] > amount else dp[amount]
 
 
