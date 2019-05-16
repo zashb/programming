298ca62feac182d1f44c310ebe8e80aa75e15f9e
bhargavayyagari <bbhaargav@gmail.com>
2019-05-16 08:17:03 -0500

    stack consolidated

--- stack_queue/len_longest_abs_file_path.py
+++ stack_queue/len_longest_abs_file_path.py
@@ -1,26 +1,38 @@
 """
 prob: find longest abs file path
-idea: stack
+idea: stack/dict, track stack len with dict
 comp:
 ex:
 """
 
 
 def longest_file_path(string):
-    dict = {}
-    longest = 0
-    fileList = string.split("\n")
-    for i in fileList:
-        if "." not in i:  # 是文件夹
-            key = i.count("\t")  # 是几级文件夹
-            value = len(i.replace("\t", ""))  # 除去\t后的长度，是实际长度
-            dict[key] = value
-        else:  # 是文件。
-            key = i.count("\t")
-            # 　文件的长度：所有目录的长度＋文件的长度＋“\”的数量
-            length = sum([dict[j] for j in dict.keys() if j < key]) + len(i.replace("\t", "")) + key
-            longest = max(longest, length)
-    return longest
+    maxlen = 0
+    pathlen = {0: 0}
+    for line in string.splitlines():
+        name = line.lstrip('\t')
+        depth = len(line) - len(name)
+        if '.' in name:
+            maxlen = max(maxlen, pathlen[depth] + len(name))
+        else:
+            pathlen[depth + 1] = pathlen[depth] + len(name) + 1
+    return maxlen
+
+    # dict = {}
+    # longest = 0
+    # fileList = string.split("\n")
+    # for i in fileList:
+    #     if "." not in i:
+    #         key = i.count("\t")
+    #         value = len(i.replace("\t", ""))
+    #         dict[key] = value
+    #     else:
+    #         key = i.count("\t")
+    #         length = sum([dict[j] for j in dict.keys() if j < key]) + len(i.replace("\t", "")) + key
+    #         longest = max(longest, length)
+    # return longest
 
 
-print(longest_file_path("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"))
+expected = 20
+actual = longest_file_path("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext")
+print(expected == actual)
