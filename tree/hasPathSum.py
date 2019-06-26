819f334022ca0a7f422fad92f6a9cf565de49137
bhargavayyagari <bbhaargav@gmail.com>
2019-06-26 06:02:17 -0500

    bin tree minor edit

--- tree/hasPathSum.py
+++ tree/hasPathSum.py
@@ -1,19 +1,19 @@
+"""
+prob: Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+comp: O(n)
+"""
+
+
 from bintree_class import TreeNode
 
 
-def hasPathSum(root, s):
-    if root is None:
-        return 0
-    else:
-        res = False
-        subSum = s - root.val
-        if root.left is None and root.right is None and subSum == 0:
-            return True
-        if root.left:
-            res = res or hasPathSum(root.left, subSum)
-        if root.right:
-            res = res or hasPathSum(root.right, subSum)
-        return res
+def hasPathSum(root, sum):
+    if not root:
+        return False
+    sum -= root.val
+    if not root.left and not root.right:  # if reach a leaf
+        return sum == 0
+    return hasPathSum(root.left, sum) or hasPathSum(root.right, sum)
 
 
 root = TreeNode(10)
