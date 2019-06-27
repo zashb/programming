f4186f1382fbfdc94e4eba66c301a8ab51b3a15f
bhargavayyagari <bbhaargav@gmail.com>
2019-06-27 07:28:41 -0500

    io-successor

--- tree/successor.py
+++ tree/successor.py
@@ -1,3 +1,9 @@
+"""
+idea: If the node has a right child, and hence its successor is somewhere lower in the tree. Go to the right once and then as many times to the left as you could. Return the node you end up with.
+Node has no right child, and hence its successor is somewhere upper in the tree. Go up till the node that is left child of its parent. The answer is the parent.
+"""
+
+
 from bintree_class import TreeNode
 
 
